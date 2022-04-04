519958af85f74878e0461b2bf9f42a15d0e1d674
jn19392 <60779279+jn19392@users.noreply.github.com>
2022-04-04 14:51:26 +0100

    Matched inflation and depression

--- prod.py
+++ prod.py
@@ -5,6 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+import tensorflow as tf
+import keras_tuner as kt
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers, metrics
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import svm
 
 # %%
 def load_data():
@@ -23,31 +30,6 @@ def load_data():
     covid_df.fillna(0)
     args += [prod_df, sentiment_df, hours_worked_df, prod_new_df, productivity_df, unem_df, covid_df]
     return args
-
-# %%
-args = load_data()
-prod_df, sentiment_df, hours_worked_df, prod_new_df, productivity_df, unem_df, covid_df = args
-for i in np.linspace(2005, 2020, 16, endpoint=True):
-    prod_df[[str(int(i))]] = prod_df[[str(int(i))]].apply(pd.to_numeric, errors="coerce")
-print(prod_df.head(), sentiment_df.head(), hours_worked_df.head(), prod_new_df.head(), productivity_df.head(), unem_df.head(), covid_df.head())
-
-#%%
-prod_data = prod_df.iloc[0:71, 2:-1].replace(np.nan, 0).to_numpy(dtype = np.float64).T
-time_data = np.repeat(np.array([np.linspace(2005, 2020, 16)]).transpose(), 71, 1)
-plt.plot(time_data, prod_data)
-plt.show()
-
-#%%
-corr = np.round(np.corrcoef(prod_df.iloc[0:25, 2:-1].replace(np.nan, 0).to_numpy(dtype = np.float16)), 3)
-mask = np.triu(np.ones_like(corr, dtype=bool))
-print(corr)
-cmap = sns.diverging_palette(230, 20, as_cmap=True)
-
-sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
-            square=True, linewidths=.5, cbar_kws={"shrink": .5})
-plt.show()
-#%%
-prod_df.head(), productivity_df.head()
 
 #%%
 def load_df(file_name):
@@ -73,6 +55,87 @@ def create_per_employeer(GDP_df, HW_df, employees_df):
         per_HW_df[i] = per_employee_df[i]/HW_df[i]
     return per_employee_df, per_HW_df
 
+
+def process_data(X, y, split_point, time_step, data_memory):
+    X_norm, X_attrs, y_norm, y_attrs = normalise(X, y)
+    split_point = int(X_norm.shape[0] *split_point)
+    y_norm = np.roll(y_norm, -time_step)
+    nrows = X_norm.shape[0]
+    samples = X_norm.shape[1]
+    X_norm = np.repeat(X_norm, data_memory, 0).reshape(nrows, data_memory, samples)
+    x_train, x_test, y_train, y_test = train_test_split(X_norm, y_norm, split_point)
+    return x_train, x_test, y_train, y_test, nrows, samples, X_norm, y_norm, X_attrs, y_attrs
+
+def normalise(X, y):
+    X_attrs = np.zeros((X.shape[-1], 2))
+    y_attrs = np.zeros((y.shape[-1], 2))
+    for i in range(X.shape[-1]):
+        X_attrs[i, :] = [np.mean(X[:, i]), np.var(X[:, i])]
+        X[:, i] = (X[:, i] - np.mean(X[:, i]))/np.var(X[:, i])**0.5
+    for i in range(y.shape[-1]):
+        y_attrs[i, :] = [np.mean(y[:, i]), np.var(y[:, i])]
+        y[:, i] = (y[:, i] - np.mean(y[:, i]))/np.var(y[:, i])**0.5  
+    return X, X_attrs, y, y_attrs
+
+def train_test_split(X, y, split_point):
+    x_train = X[:split_point, :, :]
+    x_test = X[split_point:, :, :]
+    y_train = y[:split_point]
+    y_test = y[split_point:]
+    return x_train, x_test, y_train, y_test
+        
+def create_model(layers, input_shape, print_summary):
+    model = keras.Sequential(layers)
+    model.build(input_shape=input_shape)
+    model.compile(loss='mse', optimizer='adam', metrics = [tf.keras.metrics.MeanSquaredError()])
+    if print_summary:
+        model.summary()
+    return model
+
+def run_model(X, y, time_step, data_memory, epochs, batch_size, model_layer, split_point):
+    x_train, x_test, y_train, y_test, nrows, samples, X_norm, y_norm, X_attrs, y_attrs = process_data(X, y, split_point, time_step, data_memory)
+    input_shape = (x_train.shape[0], data_memory, samples)
+    model = create_model(model_layer, input_shape, print_summary)
+    history = model.fit(x_train, y_train, validation_split = 0.1, epochs= epochs , batch_size=batch_size)
+    y_pred_norm = np.concatenate((model.predict(x_train[:, :, :]), model.predict(x_test[:, :, :])))
+    y_pred_norm = np.roll(y_pred_norm, 1, axis = 1)
+    y_pred = np.roll(y_pred_norm *y_attrs[:, 1]**0.5 + y_attrs[:, 0] , 0)
+    mse = metrics.MeanSquaredError()
+    mse.update_state(y_norm, y_pred_norm)
+    test_loss = mse.result().numpy()
+    print(test_loss)
+    train_loss = history.history['loss'][-1]
+    val_loss = history.history['val_loss'][-1]
+    model_performance = [time_step, data_memory, samples, epochs, batch_size, train_loss, val_loss, test_loss]
+    df_model = pd.DataFrame(model_performance).transpose()
+    df_model.to_csv('model_performance.csv', mode = 'a', header= False)
+    return y, y_pred, history
+
+# %%
+args = load_data()
+prod_df, sentiment_df, hours_worked_df, prod_new_df, productivity_df, unem_df, covid_df = args
+for i in np.linspace(2005, 2020, 16, endpoint=True):
+    prod_df[[str(int(i))]] = prod_df[[str(int(i))]].apply(pd.to_numeric, errors="coerce")
+print(prod_df.head(), sentiment_df.head(), hours_worked_df.head(), prod_new_df.head(), productivity_df.head(), unem_df.head(), covid_df.head())
+
+#%%
+prod_data = prod_df.iloc[0:71, 2:-1].replace(np.nan, 0).to_numpy(dtype = np.float64).T
+time_data = np.repeat(np.array([np.linspace(2005, 2020, 16)]).transpose(), 71, 1)
+plt.plot(time_data, prod_data)
+plt.show()
+
+#%%
+corr = np.round(np.corrcoef(prod_df.iloc[0:25, 2:-1].replace(np.nan, 0).to_numpy(dtype = np.float16)), 3)
+mask = np.triu(np.ones_like(corr, dtype=bool))
+print(corr)
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
+#%%
+prod_df.head(), productivity_df.head()
+#%%
 GDP_df =  load_df('./data/GDP_per_quarter_2.xlsx') * 1e6
 HW_df = load_df('./data/hours_worked.xlsx')
 employees_df = load_df('./data/Employees.xlsx') * 1e3
@@ -82,5 +145,19 @@ print(HW_df.mean(axis=1), HW_df.std(axis=1))
 
 per_HW_df.iloc[[4,6,15], :].T.plot(legend = False)
 print(per_HW_df.index)
+
 #%%
-#reg = LinearRegression().fit(np.linspace(0, len(HW_df.columns), len(HW_df.columns), endpoint=False).reshape(-1, 1), HW_df.iloc[:, 1]) 
+X = per_HW_df.to_numpy()[:, :]
+y = np.array([per_HW_df.to_numpy()[0, :]]).T
+print(X.shape, y.shape)
+#%%
+time_step, data_memory, epochs, batch_size, split_point = 5, 5, 10, 1, 0.9
+
+load_model_bool, load_model, save_model, save_fig = False, 'EU_model', 'EU_model', 'EU'
+model_layer = [layers.Dense(8, activation= 'linear'),
+                layers.LSTM(4,activation= 'sigmoid', dropout = 0.1, recurrent_dropout = 0.1, return_sequences=False),
+                layers.Dense(8, activation='linear'),
+                layers.Dense(1)]
+print_summary = True
+y, y_pred, history = run_model(X, y, time_step, data_memory, epochs, batch_size, model_layer, split_point)
+
